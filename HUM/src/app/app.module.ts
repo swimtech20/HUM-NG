1bab7a0c8f757502feaf8e857b22f5a1f7ac6197
Gagerino <gagegib@gmail.com>
2021-02-03 17:57:30 -0800

    Fixed a compile issue in app.module, add blank file for dummy data, folders for services and models (although I don't think they will be checked in since they are empty rn).

--- HUM/src/app/app.module.ts
+++ HUM/src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { HouseModule } from './modules/house/house.module';
 import { ManagementModule } from './modules/management/management.module';
+import { NgModule } from '@angular/core';
 import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HouseModule,
     ManagementModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- HUM/src/app/modules/house/house.module.ts
+++ HUM/src/app/modules/house/house.module.ts
@@ -1,14 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HouseRoutingModule } from './house-routing.module';
-
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    HouseRoutingModule
-  ]
+  imports: [CommonModule, HouseRoutingModule],
 })
-export class HouseModule { }
+export class HouseModule {}
